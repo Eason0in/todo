556a9af986d75287a83953ab32fbed4fa9038c2c
Eason <leoling127@gmail.com>
2019-04-10 17:12:23 +0800

    Add todoSeeder

---
+++ models/seeds/todoSeeder.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Todo = require('../todo.js')
+const db = mongoose.connection
+
+mongoose.connect('mongodb://127.0.0.1/todo', { useNewUrlParser: true })
+
+db.on('error', () => {
+  console.log('db is error')
+})
+
+db.once('open', () => {
+  console.log('db is connected!')
+  for (let i = 0; i < 10; i++) {
+    Todo.create({ name: 'name-' + i })
+  }
+  console.log('done')
+})
