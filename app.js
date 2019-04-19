51c4913a29ec206fcbcf0f6873dc70e45300547d
Eason <leoling127@gmail.com>
2019-04-19 13:19:41 +0800

    Add user login feature using passport and express-session

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('passport')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({ secret: 'ddd111' }))
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./config/passport')(passport)
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  next()
+})
 
 const Todo = require('./models/todo.js')
 const port = 3000
